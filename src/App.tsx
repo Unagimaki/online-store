992f0b45a4eb9ffa1d058dcf4de9c7c1a3fd48f5
Mikhail <mikhailyarkov2000@gmail.com>
2025-08-05 23:13:05 +0400

    add todo feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,45 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  type TTodo = {
+    title: string
+    completed: boolean
+    id: number
+  }
+  const [todos, setTodos] = useState<TTodo[]>([])
+  const addTodo = () => {
+    const newTodo: TTodo = {
+      title: prompt('Введите название задачи') || '',
+      completed: false,
+      id: Math.random()
+    }
+    setTodos([...todos, newTodo])
+  }
 
   return (
     <>
+
+      <h1>Todo app</h1>
+
+      <h2>Список задач:</h2>
+
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {
+          todos.map((todo) => {
+            return (
+              <div style={{display: 'flex', alignItems: 'center', gap: '1vw'}} key={todo.id}>
+                <div>{todo.title}</div>
+                <button>X</button>
+              </div>
+            )
+          })
+        }
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={addTodo}>
+        Добавить задачу
+      </button>
+
     </>
   )
 }
