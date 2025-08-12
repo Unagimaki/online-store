89973430117163842ad7db796dcd14dae7561f6d
Mikhail <mikhailyarkov2000@gmail.com>
2025-08-12 19:40:39 +0400

    added product page

--- src/app/AppRouter.tsx
+++ src/app/AppRouter.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
 import { ProductsPage } from "@/pages/ProductsPage/ui/ProductsPage";
+import { ProductPage } from "@/pages/ProductPage/ui/ProductPage";
 
 function CartPage() {
   return <div>Корзина</div>;
@@ -13,6 +14,7 @@ export function AppRouter() {
         <Route element={<Layout />}>
           <Route path="/" element={<ProductsPage />} />
           <Route path="/cart" element={<CartPage />} />
+          <Route path="/product/:id" element={<ProductPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
