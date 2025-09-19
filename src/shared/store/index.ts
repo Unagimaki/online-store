804ae7948ba45b643f918ffddf2f2be2c55c2a63
Mikhail <mikhailyarkov2000@gmail.com>
2025-09-19 23:04:17 +0400

    added query mutation order

--- src/shared/store/index.ts
+++ src/shared/store/index.ts
@@ -1,15 +1,14 @@
-// src/shared/store/index.ts
 import { configureStore } from '@reduxjs/toolkit'
 import cartReducer from '@/features/cart/model/cartSlice'
-import { orderApi } from '@/features/order/api/orderApi' // ← Импортируй API
+import { orderApi } from '@/features/order/api/orderApi' 
 
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
-    [orderApi.reducerPath]: orderApi.reducer, // ← Добавь редюсер
+    [orderApi.reducerPath]: orderApi.reducer, 
   },
     middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware().concat(orderApi.middleware), // ← Добавь middleware
+      getDefaultMiddleware().concat(orderApi.middleware), 
   })
 
 export type RootState = ReturnType<typeof store.getState>
